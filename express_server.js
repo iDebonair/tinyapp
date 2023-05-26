95bc5880f18eecf3cfb508abf2da21f3e77db6ad
iDebonair <ofemiowolabi@gmail.com>
2023-05-26 05:07:16 +0000

    Code accepts shorturl and if recognised redirects to a saved url on the database or returns error 404

--- express_server.js
+++ express_server.js
@@ -3,9 +3,30 @@ const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs"); 
 
+function generateRandomString(length) {
+  const alphanumericChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let id = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * alphanumericChars.length);
+    id += alphanumericChars[randomIndex];
+  }
+  return id;
+}
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/urls', (req, res) => {
+  const longURL = req.body.longURL;
+  const id = generateRandomString(6);
+  
+  urlDatabase[id] = longURL;
+  res.status(201).send('URL successfully saved!');
+});
+
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -28,8 +49,29 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase.b2xVn2 };
-  res.render("urls_show", templateVars);
+
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  if(longURL){
+    res.redirect(longURL)  
+  }else{
+    res.status(404).send('URL not found')
+  }
+  });
+
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL;
+  const id =  generateRandomString(6);
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls/${id}`);
+});
+
+app.post("/u/:id", (req, res) => {
+  console.log(req.body.longURL);
+  res.send("URL saved");
 });
