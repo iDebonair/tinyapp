25d0faae3fc2eb92ef8a25b48c47ef1a8c434991
iDebonair <ofemiowolabi@gmail.com>
2023-05-28 05:38:06 +0000

    Now submits updated URL, can edit 'url and can update existing URL

--- express_server.js
+++ express_server.js
@@ -12,6 +12,7 @@ function generateRandomString(length) {
   }
   return id;
 }
+
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/urls', (req, res) => {
@@ -22,12 +23,11 @@ app.post('/urls', (req, res) => {
   res.status(201).send('URL successfully saved!');
 });
 
-app.use(express.urlencoded({ extended: true }));
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
 
 app.get("/", (req, res) => {
   res.send("Hello")
@@ -49,6 +49,7 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -79,4 +80,17 @@ app.post("/u/:id", (req, res) => {
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
-})
+  res.redirect("urls");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const newURL = req.body.updatedLongURL;
+  urlDatabase[id] = newURL;
+  res.redirect("/urls"); // Redirect to the index page after editing the URL
+});
